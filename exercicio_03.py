ff3214d772c1a7fa7cbe381588fa0b0168398686
George Silva <georgeosabio@gmail.com>
2025-03-03 15:17:04 -0400

    Adicionando exercicio 03

---
+++ exercicio_03.py
@@ -0,0 +1,10 @@
+# Exercícios
+    # Inteiros (int)
+        # 03 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+num_01 = int(input("Digite o primeiro numero: "))
+num_02 = int(input("Digite o segundo numero: "))
+
+resultado = num_01 * num_02
+
+print(f"O resultado da multiplicação é: ", resultado)

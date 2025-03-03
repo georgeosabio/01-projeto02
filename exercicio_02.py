295131d6a7343319e88ebedf9189946ef54fb9b2
George Silva <georgeosabio@gmail.com>
2025-03-03 15:12:42 -0400

    Update exercicio_02.py

---
+++ exercicio_02.py
@@ -0,0 +1,8 @@
+# Exercícios
+    # Inteiros (int)
+        # 02 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero = int(input("Digite o numero: "))
+resultado = numero / 5
+
+print(f"O resto da divisão é: ", resultado)

efebcb4b3e8318b4c9e6959af2c08099c4ce325c
George Silva <georgeosabio@gmail.com>
2025-03-03 15:31:29 -0400

    Adicionando Exercicio 05

---
+++ exercicio_05.py
@@ -0,0 +1,8 @@
+# Exercícios
+    # Inteiros (int)
+        # 05 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+numero = int(input("Digite o numero: "))
+resultado = numero ** 2
+
+print("O quadrado é: ", resultado)

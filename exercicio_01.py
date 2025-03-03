f80db5bd50f195d512440510766eba9ffc195470
George Silva <georgeosabio@gmail.com>
2025-03-03 15:04:39 -0400

    Adicionando Exercicio 01

---
+++ exercicio_01.py
@@ -0,0 +1,11 @@
+# Exercícios
+    # Inteiros (int)
+        # 01 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+
+numero_01 = int(input("Digite o primeiro numero inteiro: "))
+numero_02 = int(input("Digite o segundo numero inteiro: "))
+
+resultado = numero_01 + numero_02
+
+print(f"O resultado da soma é: ", resultado)

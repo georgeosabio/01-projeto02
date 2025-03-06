704f2d43c0f01add6f42599f87c29fb90da926ff
George Silva <georgeosabio@gmail.com>
2025-03-05 20:45:28 -0400

    Adicionando Exercicio 08

---
+++ exercicio_08.py
@@ -0,0 +1,10 @@
+# Exercícios
+    # Números de Ponto Flutuante (float)
+        # 08 - Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+numero_01 = float(input("Digite o número base: "))
+numero_02 = float(input("Digite o número expoente: "))
+
+resultado = numero_01 ** numero_02
+
+print(f"O resultado da potência é: ", resultado)

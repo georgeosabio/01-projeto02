48e918ee9230c0c98d7af8e8c6f5404d4f79ffc8
George Silva <georgeosabio@gmail.com>
2025-03-05 20:13:48 -0400

    Adicionando Exercicio 06

---
+++ exercicio_06.py
@@ -0,0 +1,11 @@
+# Exercícios
+    # Números de Ponto Flutuante (float)
+        # 06 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+
+numero_01 = float(input("Digite o primeiro numero: "))
+numero_02 = float(input("Digite o segundo numero: "))
+
+resultado = numero_01 + numero_02
+
+print(f"O resultado da soma é: ", resultado)

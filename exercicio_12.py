a7acb74c7283ff08278a51e646b82aca10107b3a
George Silva <georgeosabio@gmail.com>
2025-03-06 21:16:09 -0400

    Adicionando exercicio 12

---
+++ exercicio_12.py
@@ -0,0 +1,7 @@
+# Exercícios
+    # Strings (str)
+        # 12 - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+nome = str(input("Escreva seu nome: "))
+name_slower_case = nome.lower()
+print(name_slower_case)

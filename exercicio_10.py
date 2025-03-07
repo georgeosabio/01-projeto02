071a6fcda2c18208776b2d4230474ffccfee2e44
George Silva <georgeosabio@gmail.com>
2025-03-06 21:07:35 -0400

    Update exercicio 10

--- exercicio_10.py
+++ exercicio_10.py
@@ -7,7 +7,6 @@ import math
 def calculate_circle_area(radius):
     return math.pi * (radius ** 2)
 
-# Example usage
 radius = float(input("Insira o raio do circulo: "))
 area = calculate_circle_area(radius)
 print(f"The area of the circle with radius {radius} is {area:.2f}")

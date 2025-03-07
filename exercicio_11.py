e78cdb538cda90b50ec7eddeefb91e682d1b278a
George Silva <georgeosabio@gmail.com>
2025-03-06 21:12:34 -0400

    Adicionando Exercicio 11

---
+++ exercicio_11.py
@@ -0,0 +1,7 @@
+# Exercícios
+    # Strings (str)
+        # 11 - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+
+frase = str(input("Escreva uma frase: "))
+uppercase_text = frase.upper()
+print(uppercase_text)

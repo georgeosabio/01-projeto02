a7142e1763a724f3bcb8040a3ad3222ba9937ec4
George Silva <georgeosabio@gmail.com>
2025-03-13 20:28:16 -0400

    Adicionando Exercicio 15

---
+++ exercicio_15.py
@@ -0,0 +1,12 @@
+# Exercícios
+    # Strings (str)
+        # 15 - Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+primeira_string = input("Digite a primeira string: ")
+segunda_string = input("Digite a segunda string: ")
+
+# Concatena as duas strings
+resultado = primeira_string + segunda_string
+
+# Exibe o resultado
+print("Strings concatenadas:", resultado)
